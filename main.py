2ab5ab337b7d7d94c200977e5ff6aa671f29961c
Slava Petrov <slavapmk@gmail.com>
2023-09-20 10:11:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from typing import List
 
 class Screen:
     matrix: List[List[float]]
+    width: int = 0
+    height: int = 0
 
     def __init__(self, width: int, height: int):
         self.matrix = [[0] * width] * height
+        self.width = width
+        self.height = height
 
     def render(self):
+        for y in range(self.height):
+            row = ''
+            for x in range(self.width):
+                
+
         print(json.dumps(self.matrix))
 
 
