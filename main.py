2df22ed745c4a85f78bc56121a0620e5dd424156
Slava Petrov <slavapmk@gmail.com>
2023-09-21 13:14:10 +0300

    Changed height proportion

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
-import numpy as np
+import shutil
+import time
 
+from config import MAX_FPS
+from screen import Screen
 
-class Screen:
-    matrix = [[1.0]]
-    width: int = 0
-    height: int = 0
+while True:
+    (width, height) = shutil.get_terminal_size()
+    screen = Screen(width - 1, height - 1)
 
-    def __init__(self, width: int, height: int):
-        self.matrix = np.zeros(shape=(height, width))
-        self.width = width
-        self.height = round(height / 2.57)
+    startRenderTime = time.time()
+    screen.fill(
+        -1, -1,
+        1, 1,
+        0.01
+    )
+    screen.fill(
+        -0.1, -0.1,
+        0.1, 0.1,
+        0.7
+    )
+    # screen.set_pixel(0, 0, 0.4)
+    # screen.set_pixel(-1, -1, 0.4)
+    # screen.set_pixel(1, 1, 0.4)
+    # screen.set_pixel(-1, 1, 0.4)
+    # screen.set_pixel(1, -1, 0.4)
+    # screen.set_pixel(0, -1, 0.4)
+    # screen.set_pixel(0, 1, 0.4)
+    # screen.set_pixel(1, 0, 0.4)
+    # screen.set_pixel(-1, 0, 0.4)
+    screen.render()
 
-    gradient: str = 'ÆÑÊŒØMÉËÈÃÂWQBÅæ#NÁþEÄÀHKRŽœXgÐêqÛŠÕÔA€ßpmãâG¶øðé8ÚÜ$ëdÙýèÓÞÖåÿÒb¥FDñáZPäšÇàhû§ÝkŸ®S9žUTe6µOyxÎ¾f4õ5ôú&aü™2ùçw©Y£0VÍL±3ÏÌóC@nöòs¢u‰½¼‡zJƒ%¤Itocîrjv1lí=ïì<>i7†[¿?×}*{+()\/»«•¬|!¡÷¦¯—^ª„”“~³º²–°¹‹›;:’‘‚’˜ˆ¸…·¨´` '
-
-    def parse_pixel(self, pixel: float):
-        return self.gradient[round((len(self.gradient) - 1) * (1 - pixel))]
-
-    def set_pixel(self, x: float, y: float, value: float):
-        self.matrix[round((x + 1) / 2 * (self.width - 1))][round((y + 1) / 2 * (self.height - 1))] = value
-
-    def fill(self, x_start: float, y_start: float, x_stop: float, y_stop: float, value: float):
-        start_screen_x = round((x_start + 1) / 2 * (self.width - 1))
-        start_screen_y = round((y_start + 1) / 2 * (self.height - 1))
-        stop_screen_x = round((x_stop + 1) / 2 * (self.width - 1))
-        stop_screen_y = round((y_stop + 1) / 2 * (self.height - 1))
-        for screen_x in range(start_screen_x, stop_screen_x + 1):
-            for screen_y in range(start_screen_y, stop_screen_y + 1):
-                self.matrix[screen_x][screen_y] = value
-
-    def render(self):
-        canvas = ''
-        for y in range(self.height):
-            for x in range(self.width):
-                canvas += self.parse_pixel(self.matrix[x][y])
-            canvas += '\n'
-        print(canvas)
-
-
-screen = Screen(51, 51)
-screen.fill(
-    -1, -1,
-    1, 1,
-    0.01
-)
-screen.set_pixel(0, 0, 0.4)
-screen.set_pixel(-1, -1, 0.4)
-screen.set_pixel(1, 1, 0.4)
-screen.set_pixel(-1, 1, 0.4)
-screen.set_pixel(1, -1, 0.4)
-screen.set_pixel(0, -1, 0.4)
-screen.set_pixel(0, 1, 0.4)
-screen.set_pixel(1, 0, 0.4)
-screen.set_pixel(-1, 0, 0.4)
-screen.render()
+    renderDuration = time.time() - startRenderTime
+    time_ = (1 / MAX_FPS) - renderDuration
+    if time_ > 0:
+        time.sleep(time_)
