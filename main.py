2838c080a00a8cf5d7174279732763cc0b566068
Slava Petrov <slavapmk@gmail.com>
2023-09-21 16:11:09 +0300

    Example fix

--- main.py
+++ main.py
@@ -22,15 +22,14 @@ while True:
         0.1, 0.1,
         random.randrange(30, 80) / 100
     )
-    screen.set_pixel(0, 0, 0.4)
-    screen.set_pixel(-1, -1, 0.4)
-    screen.set_pixel(1, 1, 0.4)
-    screen.set_pixel(-1, 1, 0.4)
-    screen.set_pixel(1, -1, 0.4)
-    screen.set_pixel(0, -1, 0.4)
-    screen.set_pixel(0, 1, 0.4)
-    screen.set_pixel(1, 0, 0.4)
-    screen.set_pixel(-1, 0, 0.4)
+    screen.set_pixel(-0.3, -0.3, 0.4)
+    screen.set_pixel(0.3, 0.3, 0.4)
+    screen.set_pixel(-0.3, 0.3, 0.4)
+    screen.set_pixel(0.3, -0.3, 0.4)
+    screen.set_pixel(0, -0.3, 0.4)
+    screen.set_pixel(0, 0.3, 0.4)
+    screen.set_pixel(0.3, 0, 0.4)
+    screen.set_pixel(-0.3, 0, 0.4)
     render = screen.render()
 
     renderDuration = time.time() - startRenderTime
